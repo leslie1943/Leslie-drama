ab1e84bde86386f286332008b9899ace049b0f19
苏震 <leslie43@sina.com>
2019-02-18 23:07:51 +0800

    remove dist
    remove dist

--- dist/js/chunk-2d2300c6.ee0ed591.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2300c6"],{eb2e:function(e,t,n){"use strict";n.r(t);var o=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("d2-container",[n("template",{slot:"header"},[e._v("Page 1 header")]),e._v("Hello World\n  "),n("template",{slot:"footer"},[e._v("footer")])],2)},l=[],a={name:"page1"},r=a,c=n("2877"),s=Object(c["a"])(r,o,l,!1,null,null,null);s.options.__file="index.vue";t["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d2300c6.ee0ed591.js.map
