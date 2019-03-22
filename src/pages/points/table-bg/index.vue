ed7e85ff0e99d3ebc5fda8c09f4b1a54a2296cb4
苏震 <leslie43@sina.com>
2019-03-22 19:05:58 +0800

    Update index.vue

--- src/pages/points/table-bg/index.vue
+++ src/pages/points/table-bg/index.vue
@@ -44,55 +44,18 @@ export default {
   data () {
     return {
       tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
+        { date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄' },
+        { date: '2016-05-04', name: '王小虎', address: '上海市普陀区金沙江路 1517 弄' },
+        { date: '2016-05-01', name: '王小虎', address: '上海市普陀区金沙江路 1519 弄' },
+        { date: '2016-05-03', name: '王小虎', address: '上海市普陀区金沙江路 1516 弄' }
       ],
-      tableDataWithMerge: [{
-        id: '12987122',
-        name: '王小虎',
-        amount1: '234',
-        amount2: '3.2',
-        amount3: 10
-      }, {
-        id: '12987123',
-        name: '王小虎',
-        amount1: '165',
-        amount2: '4.43',
-        amount3: 12
-      }, {
-        id: '12987124',
-        name: '王小虎',
-        amount1: '324',
-        amount2: '1.9',
-        amount3: 9
-      }, {
-        id: '12987125',
-        name: '王小虎',
-        amount1: '621',
-        amount2: '2.2',
-        amount3: 17
-      }, {
-        id: '12987126',
-        name: '王小虎',
-        amount1: '539',
-        amount2: '4.1',
-        amount3: 15
-      }]
+      tableDataWithMerge: [
+        { id: '12987122', name: '王小虎', amount1: '234', amount2: '3.2', amount3: 10 },
+        { id: '12987123', name: '王小虎', amount1: '165', amount2: '4.43', amount3: 12 },
+        { id: '12987124', name: '王小虎', amount1: '324', amount2: '1.9', amount3: 9 },
+        { id: '12987125', name: '王小虎', amount1: '621', amount2: '2.2', amount3: 17 },
+        { id: '12987126', name: '王小虎', amount1: '539', amount2: '4.1', amount3: 15 }
+      ]
     }
   },
   methods: {
